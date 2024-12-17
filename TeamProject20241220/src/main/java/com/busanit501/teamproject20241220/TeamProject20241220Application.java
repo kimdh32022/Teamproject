8c454a37be62cceaed0b2ef8adc963ccfc6e8b57
kimdh32022 <kimdh32022@naver.com>
2024-12-17 13:48:03 +0900

    최초 커밋

---
+++ TeamProject20241220/src/main/java/com/busanit501/teamproject20241220/TeamProject20241220Application.java
@@ -0,0 +1,13 @@
+package com.busanit501.teamproject20241220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProject20241220Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProject20241220Application.class, args);
+    }
+
+}
---
+++ TeamProject20241220/src/test/java/com/busanit501/teamproject20241220/TeamProject20241220ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit501.teamproject20241220;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject20241220ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
